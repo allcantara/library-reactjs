5b292397133345f485dd990a7cf0f17d6ee8bccb
allcantara <alcantara.desenvolvedor@gmail.com>
2020-04-25 12:35:12 -0300

    Correçao no width do input

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -14,7 +14,7 @@ export const Container = styled.div`
 export const InputComponent = styled.input`
   @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
   max-width: 100%;
-  width: ${({ width }: Props) => width && width + 'px'};
+  width: ${({ width }: Props) => width ? width + 'px' : '100%'};
   height:  ${({ height }: Props) => height + 'px'};
   background-color: ${({ backgroundColor }: Props) => backgroundColor};
   border-color: ${({ borderColor }: Props) => borderColor};
